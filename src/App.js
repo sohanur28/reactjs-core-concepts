33e85053d3e9a90552c63e325f83fd63efe0a19a
sohanur28 <gsrahman28@gmail.com>
2023-03-26 22:36:15 +0600

    rec

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 5555;
+const singer = {name: 'Dr. Mahfuz', job: 'Singer'}
+const singer2 = {name: 'Eva Rahman', job: 'Singer2'}
+
+const singerStyle= {
+  color: 'purple',
+  backgroundColor: 'white'
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>JSX</h1>
+        <div className="container">
+          <h3>Hello Dude. React. How are you?</h3>
+        </div>
+        <div className="music">
+          <p>Name: {2222 + number}</p>
+          <p style={singerStyle}>Name: {singer.name} {singer.job}</p>
+          <p>Name: {singer2.name} {singer2.job}</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
