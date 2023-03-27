c9b1b5565062739fd56356839193e974e8f7ca68
sohanur28 <gsrahman28@gmail.com>
2023-03-27 14:56:37 +0600

    createComponent

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 const number = 5555;
-const singer = {name: 'Dr. Mahfuz', job: 'Singer'}
-const singer2 = {name: 'Eva Rahman', job: 'Singer2'}
+const singer = { name: 'Dr. Mahfuz', job: 'Singer' }
+const singer2 = { name: 'Eva Rahman', job: 'Singer2' }
 
-const singerStyle= {
+const singerStyle = {
   color: 'purple',
   backgroundColor: 'white'
 }
@@ -13,29 +13,33 @@ const singerStyle= {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello Dude. React. How are you?</h3>
-        </div>
-        <div className="music">
-          <p>Name: {2222 + number}</p>
-          <p style={singerStyle}>Name: {singer.name} {singer.job}</p>
-          <p>Name: {singer2.name} {singer2.job}</p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pesrson></Pesrson>
+      <Pesrson></Pesrson>
+      <Pesrson></Pesrson>
+      <Pesrson></Pesrson>
+      <h5>New component. YAY</h5>
+      <p>rock mama React mama</p>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
+    </div>
+  );
+}
+
+function Pesrson() {
+  return (
+    <div className='person'>
+      <h1>Sakib Al Hasan</h1>
+      <p>Profession: Cricket</p>
+    </div>
+  );
+}
+
+function Friend(){
+  return (
+    <div className='container'>
+      <h3>Name: Ajay Devgun</h3>
+      <p>Job: maramari</p>
     </div>
   );
 }
